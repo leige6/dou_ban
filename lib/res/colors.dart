3a28827b293464db8c9e2b4135c904cc129877b5
yourleige@gmail.com <Fang201314>
2019-03-21 13:16:30 +0800

    初始化项目

---
+++ lib/res/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colours  {
+  static const Color  app_clear = Color(0xFF6D6D6D);
+  static const Color  app_login_top_info = Color(0xFF7C7C7C);
+  static const Color  app_login_back = Color(0x00FAFAFA);
+
+
+}

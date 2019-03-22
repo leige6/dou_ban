c2995d6df28635038c9d758b2330f755f2592b6c
yourleige@gmail.com <Fang201314>
2019-03-22 17:34:56 +0800

    listview上拉刷新和下拉加载优化

--- lib/movie/movie_list_page.dart
+++ lib/movie/movie_list_page.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:dou_ban/model/movie.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_refresh/flutter_refresh.dart';
 
 class MovieListPage extends StatefulWidget {
   @override
@@ -11,88 +11,69 @@ class MovieListPage extends StatefulWidget {
 
 class MovieListPageState extends State<MovieListPage> with AutomaticKeepAliveClientMixin{
 
-  GlobalKey<EasyRefreshState> _easyRefreshKey = new GlobalKey<EasyRefreshState>();
-  GlobalKey<RefreshHeaderState> _headerKey = new GlobalKey<RefreshHeaderState>();
-  GlobalKey<RefreshFooterState> _footerKey = new GlobalKey<RefreshFooterState>();
-  var start=0;
-  List<Movie> movies = [];
-
+  int start=0;
+  List<dynamic> movies = [];
   @override
   bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
-    //一进页面就请求接口，一开始不知道这个 initState 方法 ，折腾了很久
     getMovieListData();
   }
 
+
+  Future<Null> onFooterRefresh() {
+    return new Future.delayed(new Duration(seconds: 1), () {
+      setState(() {
+        start += 10;
+        getMovieListData();
+      });
+    });
+  }
+
+
+  Future<Null> onHeaderRefresh() {
+    return new Future.delayed(new Duration(seconds: 1), () {
+      setState(() {
+        start = 0;
+        getMovieListData();
+      });
+    });
+  }
 
 
   @override
   Widget build(BuildContext context) {
     super.build(context);//AutomaticKeepAliveClientMixin起作用必须添加
     return Scaffold(
-      body: Center(
-          child: new EasyRefresh(
-            key: _easyRefreshKey,
-            autoLoad: false,
-            behavior: ScrollOverBehavior(),
-            refreshHeader: ClassicsHeader(
-              key: _headerKey,
-              refreshText: '下拉刷新',
-              refreshReadyText:'松开刷新',
-              refreshingText: '加载中' + "...",
-              refreshedText: '更新中',
-              moreInfo:'更新中',
-              bgColor: Colors.transparent,
-              textColor: Colors.black87,
-              moreInfoColor: Colors.black54,
-              showMore: true,
-            ),
-            refreshFooter: ClassicsFooter(
-              key: _footerKey,
-              loadText: '下拉加载',
-              loadReadyText: '松开刷新',
-              loadingText: '加载中',
-              loadedText:'加载完成',
-              noMoreText: '加载完成',
-              moreInfo: '下拉加载更多',
-              bgColor: Colors.transparent,
-              textColor: Colors.black87,
-              moreInfoColor: Colors.black54,
-              showMore: true,
-            ),
-            child: new ListView.builder(
-              //ListView的Item
-                itemCount: movies.length,
-                itemBuilder: (BuildContext context,int index){
-                  return buildMovieItems(index);
-                }
-            ),
-            onRefresh: () async{
-              await new Future.delayed(const Duration(seconds: 0), () {
-                setState(() {
-                  start=0;
-                  getMovieListData();
-                });
-              });
+        body: Center(
+          child: new Refresh(
+            onFooterRefresh: onFooterRefresh,
+            onHeaderRefresh: onHeaderRefresh,
+            childBuilder: (BuildContext context,
+                {ScrollController controller, ScrollPhysics physics}) {
+              return new Container(
+                  child: new ListView.builder(
+                    physics: physics,
+                    controller: controller,
+                    itemCount: movies.length,
+                    itemBuilder: (context, item) {
+                      return buildMovieItems(item);
+                    },
+                  ));
             },
-            loadMore: () async {
-              await new Future.delayed(const Duration(seconds: 0), () {
-                  setState(() {
-                    start+=10;
-                    getMovieListData();
-                  });
-              });
-            },
-          )
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ),
+
     );
   }
 
+
+
+
   //展示数据内容
-  buildMovieItems(var index) {
+  Widget  buildMovieItems(var index) {
       Movie movie = movies[index];
       var movieImage = new Padding(
         padding: const EdgeInsets.only(
@@ -156,7 +137,7 @@ class MovieListPageState extends State<MovieListPage> with AutomaticKeepAliveCli
     print('点击电影：'+movie.title);
   }
 
-  //获取数据内容
+
   getMovieListData() async {
     print("当前页数："+start.toString());
     var httpClient = new HttpClient();
